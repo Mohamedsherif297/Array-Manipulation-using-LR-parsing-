b279c131e195e05fc7e31738f367cea17737c849
Kkarim-Ahmed <kkarim.1.aahmed@gmail.com>
2026-04-11 22:46:32 +0200

    Irrelivant

---
+++ Item.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Item {
+    string lhs;
+    vector<string> rhs;
+    int dotPos;
+    string lookahead;
+
+    bool operator<(const Item& other) const {
+        if (lhs != other.lhs) return lhs < other.lhs;
+        if (rhs != other.rhs) return rhs < other.rhs;
+        if (dotPos != other.dotPos) return dotPos < other.dotPos;
+        return lookahead < other.lookahead;
+    }
+
+    bool operator==(const Item& other) const {
+    return lhs == other.lhs &&
+           rhs == other.rhs &&
+           dotPos == other.dotPos &&
+           lookahead == other.lookahead;
+    }
+
+string nextSymbol() const {
+    if (dotPos < rhs.size())
+        return rhs[dotPos];
+    return "";
+    }
+
+    bool isComplete() const {
+    return dotPos >= rhs.size();
+    }
+
+    Item advanceDot() const {
+    Item next = *this;
+    next.dotPos++;
+    return next;
+    }
+
+    void print() const {
+    cout << lhs << " → ";
+    for (int i = 0; i < rhs.size(); i++) {
+        if (i == dotPos) cout << "• ";
+        cout << rhs[i] << " ";
+    }
+    if (dotPos == rhs.size()) cout << "• ";
+    cout << ", " << lookahead << endl;
+ }
+};
--- To_do_list.md
+++ To_do_list.md
@@ -1,32 +1,24 @@
-___________Syntax Analysis___________
+1. Item struct (complete with operators + helpers) ✅
 
-1. Item struct      // it is (.) a dot operand added to tell the parser where to look and where we are.
+2. FIRST set ✅
+2.1 firstOfSequence() (CRITICAL)
 
-2. FIRST & FOLLOW Sets      {Research}
-// Meaning and how to do it?
+3. Complete LR(1) Item struct (with operators + helpers)
 
-3. Represent LR(1) Items {Research}
-// SLR may be easier to do than LR(1)
+4. Closure()
 
-4. Closure      {Research}
-// Meaning and how to do it?
+5. GOTO()
 
-5. Implement GOTO()      {Research}
-// Meaning and how to do it?
+6. Build Parser States (Canonical Collection)
 
-6. Detect conflicts
+7. Build Parsing Tables (ACTION + GOTO)
 
-7. Build Parser States (Canonical Collection)      {Research}
-
-8. Build Parsing Tables (ACTION + GOTO)      {Research}
+8. Detect conflicts
 
 9. Implement Shift-Reduce Parser
 
-10. Parse Tree
+10. Parse Tree (optional)
 
-11. Syntax Error Handling      {Research}
-// What errors need to be caught for the array manipulation?
+11. Syntax Error Handling
 
 12. Connect Lexer → Parser
-
---------------------
